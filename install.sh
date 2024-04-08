a788287221d683069757b4317b786a1fc8becf4b
r2dd <>
2024-04-08 17:47:07 +0200

    FIX: install script didn't create tutorials directory

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ pip install mkdocs
 pip install mkdocstrings
 pip install mkdocstrings-python
 pip install jupytext
+mkdir site/tutorials
 jupytext --to md tutorials/*py; mv tutorials/*md site/tutorials; mkdocs build
 jupytext --to ipynb tutorials/*py
