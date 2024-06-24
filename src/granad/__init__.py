2703ab5cc84c7b6e2df76330686506f310acf111
r2dd <david.dams@tutanota.com>
2024-06-24 19:33:29 +0200

    FIX: export

--- src/granad/__init__.py
+++ src/granad/__init__.py
@@ -4,6 +4,6 @@ warnings.filterwarnings('ignore')
 
 from granad.fields import *
 from granad.materials import *
-from granad._numerics import fermi, fraction_periodic, get_fourier_transform
+from granad._numerics import fermi, fraction_periodic, get_fourier_transform, get_coulomb_field_to_from
 from granad.orbitals import *
 from granad.shapes import *
