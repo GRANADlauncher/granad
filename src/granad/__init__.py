c9cac163c0f763aa9cd1aca6b64d1aab009e75dc
r2dd <david.dams@tutanota.com>
2024-08-08 11:43:30 +0200

    MAINT: exporting potentials

--- src/granad/__init__.py
+++ src/granad/__init__.py
@@ -3,6 +3,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 from granad.fields import *
+from granad.potentials import *
 from granad.materials import *
 from granad._numerics import fermi, fraction_periodic, get_fourier_transform, get_coulomb_field_to_from
 from granad.orbitals import *
