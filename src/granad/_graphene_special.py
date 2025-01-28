0a37e04caeedebb10b11a235a29092d696e90733
Abhishek Ghosh <ov.021098@gmail.com>
2025-01-28 17:36:03 +0100

    Corrected the graphene cutting function

--- src/granad/_graphene_special.py
+++ src/granad/_graphene_special.py
@@ -2,15 +2,15 @@ import jax
 import jax.numpy as jnp
 from matplotlib.path import Path
 
-def get_polygon(n, edge_type, side_length: float, min_side_length: float, side_length_increment: float, shift_vec_for_edge_alignment) -> jax.Array:
+def get_polygon(n: Literal[3,6], side_length: float, orientation_angle: float, min_side_length: float, side_length_increment: float, shift_vec_for_edge_alignment: Callable[[int],jax.Array]) -> jax.Array:
     """
     Creates a regular triangle or hexagon of suitable side length (<= given side length) containing maximum number of hexagon (from lattice) per side.
     Then it put the shape on top of the big material sheet and orient the polygon as per edge type requriment.
     
     Parameters:
         n (int): Order of the polygon. Only 3 and 6 are allowed.
-        edge_type (Literal['AC','ZZ']): Specifies Arm Chair or Zig Zag type edge.
         side_length (float): Side length of the polygon,the user wants to build. (Angstrom) 
+        orientation_angle: Rotates the polygon during creation.
         min_side_length (float): Minimum possible side length of the polygon depending on the shape (triangle or hexagon) and edge type of the flake to be cut.
         side_length_increment (float): Quantum of side length increment if one more hexagon (from lattice) is to be included in the flake.
         shift_vec_for_edge_alignment (Callable[[int],jax.Array]): Depending on odd or even number of hexagon present per side,
@@ -20,14 +20,15 @@ def get_polygon(n, edge_type, side_length: float, min_side_length: float, side_l
         
     """
     ##  Side length increases as integer multiple of side_length_increment ==> 0,1,2,3...
-    k=jnp.abs(side_length-min_side_length)//side_length_increment
+    k=0 if side_length<=min_side_length else (side_length-min_side_length)//side_length_increment
     side_length=min_side_length+k*side_length_increment
     radius=side_length/(2*jnp.cos(jnp.pi/2*(1-2/n)))
-    if (edge_type,n) in [('AC',6), ('ZZ',3)] : 
-        polygon=jax.vmap(lambda theta : jnp.array([radius*jnp.cos(theta),radius*jnp.sin(theta),0]))(jnp.linspace(0,2*jnp.pi,n+1)[:-1])
-    elif (edge_type,n) in [('ZZ',6), ('AC',3)] :
-        polygon=jax.vmap(lambda theta : jnp.array([radius*jnp.sin(theta),radius*jnp.cos(theta),0]))(jnp.linspace(0,2*jnp.pi,n+1)[:-1])
-    #The created polygon may not be on top of the sheet of material
+    phi=orientation_angle
+    R=jnp.array([[jnp.cos(phi),-jnp.sin(phi),0],
+                [jnp.sin(phi),jnp.cos(phi),0],
+                [0,0,1]])
+    polygon=jax.vmap(lambda theta : jnp.array([radius*jnp.sin(theta+phi),radius*jnp.cos(theta+phi),0]))(jnp.linspace(0,2*jnp.pi,n+1)[:-1])
+    #The created polygon may not be on top of the sheet of materialmin_x_polygon, min_y_polygon=jnp.min(polygon[:,0]), jnp.min(polygon[:,1])
     #We assume the the bottom-left lattice point of the rectangular material sheet is kept at origin (0,0,0)
     min_x_polygon, min_y_polygon=jnp.min(polygon[:,0]), jnp.min(polygon[:,1])
     #Shift the polygon such that it alings perfectly with the sheet
@@ -39,7 +40,7 @@ def get_polygon(n, edge_type, side_length: float, min_side_length: float, side_l
 # Define a rectangular graphene sheet
 def get_graphene_sheet(m,n,lattice_const):
     def translation_vector(m,n):
-        return jnp.array([3*lattice_const*m,jnp.sqrt(3*lattice_const)*n,0])
+        return jnp.array([3*lattice_const*m,jnp.sqrt(3)*lattice_const*n,0])
     
     unit_cell=jnp.array([[0,0,0],
           [lattice_const/2,jnp.sqrt(3)/2*lattice_const,0],
@@ -51,23 +52,26 @@ def get_graphene_sheet(m,n,lattice_const):
     coords=jax.vmap(lambda m,n: shift+unit_cell+translation_vector(m,n),in_axes=(0,0))(flatten_idx_grid[:,0],flatten_idx_grid[:,1]).reshape(m*n*unit_cell.shape[0],3)
     return coords-coords[0] # To make the bottom-left lattice coordinate (0,0,0)
 
+
+
 def _cut_flake_graphene(polygon_id, edge_type, side_length, lattice_constant):
     # measure everything in units of the lc
-    side_length /= lattice_constant
-    a = 1
+    a = lattice_constant
     mapping = {
-    "triangle_armchair" : ["AC", 3, 6*a, 3*a, lambda k: jnp.array([1,0,0])*a],
-    "triangle_zigzag" : ["ZZ", 3, 3*jnp.sqrt(3)*a, jnp.sqrt(3)*a, lambda k: jnp.array([0,0,0])*a],
-    "hexagon_armchair" : ["AC", 6, 4*a, 3*a, lambda k: jnp.array([0,jnp.sqrt(3)/4*(1-(-1)**k),0])],
-    "hexagon_zigzag" : ["ZZ", 6, (jnp.sqrt(3)+2/jnp.sqrt(3))*a, jnp.sqrt(3)*a, lambda k: jnp.array([0 ,(1-3*(-1)**k)*a/(4*jnp.sqrt(3)),0])]                
+    "triangle_armchair" : [3, side_length, 0, 6*a, 3*a, lambda k: jnp.array([1,0,0])*a],
+    "triangle_zigzag" : [3, side_length, jnp.pi/2, 3*jnp.sqrt(3)*a, jnp.sqrt(3)*a, lambda k: jnp.array([0,0,0])*a],
+    "hexagon_armchair" : [6, side_length, jnp.pi/2, 4*a, 3*a, lambda k: jnp.array([0,jnp.sqrt(3)/4*(1-(-1)**k),0])],
+    "hexagon_zigzag" : [6, side_length, 0, (jnp.sqrt(3)+2/jnp.sqrt(3))*a, jnp.sqrt(3)*a, lambda k: jnp.array([0 ,(1-3*(-1)**k)*a/(4*jnp.sqrt(3)),0])]                
                 }
 
-    edge_type, n, min_side_length, side_length_increment, shift_vec_for_edge_alignment = mapping[f"{polygon_id}_{edge_type}"]
-    polygon=get_polygon(n, edge_type, side_length, min_side_length, side_length_increment, shift_vec_for_edge_alignment)     
+    n, orientation_angle, min_side_length, side_length_increment, shift_vec_for_edge_alignment = mapping[f"{polygon_id}_{edge_type}"]
+    polygon=get_polygon(n, side_length, orientation_angle, min_side_length, side_length_increment, shift_vec_for_edge_alignment)     
     path = Path(polygon[:,:2])
-    m=jnp.int32((jnp.max(polygon[:,0])-jnp.min(polygon[:,0]))/(3*a)+1)
-    n=jnp.int32((jnp.max(polygon[:,1])-jnp.min(polygon[:,1]))/jnp.sqrt(3*a)+1)
+    width=jnp.max(polygon[:,0])-jnp.min(polygon[:,0])
+    height=jnp.max(polygon[:,1])-jnp.min(polygon[:,1])
+    m=jnp.int32(width/(3*a)+2)
+    n=jnp.int32(height/(jnp.sqrt(3)*a)+2)
     sheet=get_graphene_sheet(m, n, lattice_const=a)
     inside = path.contains_points(sheet[:,:2],radius=-0.01*a) # Watch the -ve sign here
     polygon = jnp.vstack([polygon[:, :2], polygon[0, :2]])
-    return m, n, lattice_constant * polygon, lattice_constant * sheet[inside], lattice_constant * sheet
+    return m, n,  polygon, sheet[inside],  sheet
