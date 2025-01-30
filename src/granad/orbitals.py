c70269ded775bda19c7e6f35d69bfd23e706fd35
r2dd <david.dams@tutanota.com>
2025-01-30 11:27:07 +0100

    fix: doc string finally [build-docs]

--- src/granad/orbitals.py
+++ src/granad/orbitals.py
@@ -237,7 +237,7 @@ class TDResult:
                                             axis is returned. Otherwise, only the Fourier transformed data is returned.
 
         """
-        return _numerics.get_fourier_transform(self.time_axis, self.td_illumination.real, omega_max, omega_min, return_omega_axis)
+        return _numerics.get_fourier_transform(self.time_axis, self.td_illumination, omega_max, omega_min, return_omega_axis)
 
     def save( self, name ):
         jnp.savez(f"{name}.npz", **asdict(self) )
@@ -1324,7 +1324,7 @@ class OrbitalList:
         """independent-particle greens function at the specified frequency according to 
 
         $$
-        G_{AB}(\omega) = \sum_{nm} \frac{P_m - P_n}{\omega + E_m - E_n + i e} A_{nm} B_{mn}
+        G_{AB}(\omega) = \sum_{nm} \\frac{P_m - P_n}{\omega + E_m - E_n + i e} A_{nm} B_{mn}
         $$
 
         Parameters: 
