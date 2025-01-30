c58cc73a024610da6b48523ab7748bb7bb8e913c
karolina@fizyka.umk.pl <karolina@fizyka.umk.pl>
2025-01-30 15:04:18 +0100

    removed CorrectedLindblad aka Miriam's dissipation model [build-docs]

--- src/granad/dissipators.py
+++ src/granad/dissipators.py
@@ -30,26 +30,4 @@ def SaturationLindblad(saturation):
 
     return inner
 
-def CorrectedLindblad(saturation):
-    """Function for modelling dissipation according to a corrected Lindblad model, replacing the density matrix with an "out-of-equilibrium" derivation $\\rho - \\rho_0$
-    """
-    saturation = jax.vmap(saturation, 0, 0)
 
-    def inner(t, r, args):
-        # convert rho to energy basis
-        r = args.eigenvectors.conj().T @ (r - args.stationary_density_matrix) @ args.eigenvectors
-
-        # extract occupations
-        diag = jnp.diag(r) * args.electrons
-
-        # apply the saturation functional to turn off elements in the gamma matrix
-        gamma = args.relaxation_rate.astype(complex) * saturation(diag)[None, :]
-
-        a = jnp.diag(gamma.T @ jnp.diag(r))
-        mat = jnp.diag(jnp.sum(gamma, axis=1))
-        b = -1 / 2 * (mat @ r + r @ mat)
-        val = a + b
-
-        return args.eigenvectors @ val @ args.eigenvectors.conj().T
-
-    return inner
