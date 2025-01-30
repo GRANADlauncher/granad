e54a7ddf50362da213bf50c01a69685d17ec405f
karolina@fizyka.umk.pl <karolina@fizyka.umk.pl>
2025-01-30 15:09:35 +0100

    modified the description of SaturationLindblad[build-docs]

--- src/granad/dissipators.py
+++ src/granad/dissipators.py
@@ -8,7 +8,7 @@ def DecoherenceTime():
 
 def SaturationLindblad(saturation):
     """Function for modelling dissipation according to the saturated lindblad equation as detailed in [Pelc et al.](https://link.aps.org/doi/10.1103/PhysRevA.109.022237).
-    The argument stands for the saturation functional.
+    The argument stands for the saturation functional. If identity is selected as the saturation functional, the model represents the canonical Lindblad relaxation.
     """
     saturation = jax.vmap(saturation, 0, 0)
 
