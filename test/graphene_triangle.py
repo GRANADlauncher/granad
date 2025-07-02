b6609ad3b4b7403e93bbb9df472c9337dd2e001d
r2dd <david.dams@tutanota.com>
2025-07-02 16:31:26 +0200

    feat,test(triangle): spin-resolution

--- test/graphene_triangle.py
+++ test/graphene_triangle.py
@@ -38,16 +38,31 @@ shape = Triangle(24, armchair = False)
 flake = graphene_spinful.cut_flake(shape) 
 flake.set_open_shell()
 flake.set_electrons(len(flake)//2)
-flake.show_2d()
+# flake.show_2d()
 
 rho_0 = jnp.ones(len(flake)) 
 rho_0 = jnp.diag(rho_0).astype(complex) 
 flake.set_mean_field(mix = 0.4, iterations = 200, rho_0 = rho_0)
-flake.show_energies(e_min = -1., e_max = 4)
+
+# spin sectors decouple, so diagonalize separately and argsort 
+N = len(flake) // 2
+engs_up, _ = jnp.linalg.eigh(flake.hamiltonian[:N, :N])
+engs_down, _ = jnp.linalg.eigh(flake.hamiltonian[N:, N:])
+energies_by_sector = jnp.concatenate([engs_up, engs_down])
+spins = jnp.concatenate([jnp.ones(len(flake)//2), -jnp.ones(len(flake)//2)])
+idxs = jnp.argsort(energies_by_sector)
+
+# sanity check
+assert jnp.all(jnp.abs(energies_by_sector[idxs] - flake.energies) < 1e-14)
+
+# plotting
+spins = spins[idxs]
+flake.show_energies(e_min = -1., e_max = 4, display = spins, label = "spin", name = "energies.pdf")
+
 
 occs = jnp.diag(flake.initial_density_matrix) * flake.electrons
 spin_density = occs[:len(flake)//2] - occs[len(flake)//2:]
-show_2d(flake[:len(flake)//2], display = spin_density)
+show_2d(flake[:len(flake)//2], display = spin_density, name = "spin_density.pdf", mode = "two-signed")
 
 
 sublattice_diff = len(flake.filter_orbs("sublattice_1+", Orbital)) -  len(flake.filter_orbs("sublattice_2+", Orbital))
