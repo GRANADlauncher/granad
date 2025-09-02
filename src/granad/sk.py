3f59960549cc56c42ef30cf820b1cd6161201608
r2dd <david.dams@tutanota.com>
2025-09-02 10:20:55 +0200

    chore: sk sketch

---
+++ src/granad/sk.py
@@ -0,0 +1,8 @@
+# idea
+# sk = SKparams(ppsigma = [] | func, ....), flake.set_coupling_slater_koster(sk)
+# flake.set_orbital_group_type(Orbital, SKParams.pz) # expand all orbitals of same type
+# 
+
+Sk = SKParams(
+    
+)
